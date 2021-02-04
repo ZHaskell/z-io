1cbc13f6f5f624f8e121785fd445e5d70c344e83
Dong <winterland1989@gmail.com>
2021-02-04 18:52:53 +0800

    upd module

--- Z/IO/FileSystem.hs
+++ Z/IO/FileSystem.hs
@@ -1,3 +1,16 @@
+{-|
+Module      : Z.IO.Network
+Description : FileSystem Umbrella module
+Copyright   : (c) Song Xue, Dong Han, 2020
+License     : BSD
+Maintainer  : winterland1989@gmail.com
+Stability   : experimental
+Portability : non-portable
+
+Umbrella module to export everything you need to start using file.
+
+-}
+
 module Z.IO.FileSystem
   ( -- * Basic Operations
     module Z.IO.FileSystem.Base
